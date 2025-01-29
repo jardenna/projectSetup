dc2953d0360b6bc6d5dc9be6ca88686555937f22
jardenna <hbjornum@gmail.com>
2025-01-29 11:17:36 +0100

    eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -45,13 +45,23 @@ export default tseslint.config(
         },
       },
     },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
     rules: {
+      'prettier/prettier': [
+        'error',
+        { endOfLine: 'auto' },
+        { usePrettierrc: true },
+      ], // Use our .prettierrc file as source
       '@typescript-eslint/no-unused-vars': 'warn',
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-duplicate-enum-values': 'warn',
       '@typescript-eslint/no-empty-interface': 'warn',
-      '@typescript-eslint/no-var-requires': 'off',
-      '@typescript-eslint/no-empty-interface': 'off',
 
       'import/no-extraneous-dependencies': [
         'error',
@@ -77,6 +87,7 @@ export default tseslint.config(
       'react/jsx-uses-react': 'off',
       'react/react-in-jsx-scope': 'off',
       'react/prop-types': 'off',
+      'jsx-a11y/alt-text': 'error',
       'react/jsx-filename-extension': [
         2,
         { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
--- src/Input.tsx
+++ src/Input.tsx
@@ -4,10 +4,18 @@ interface InputProps {
   text?: string;
 }
 
-const Input: FC<InputProps> = ({ text }) => (
-  <section>
-    <button>jj {text}</button>
-  </section>
-);
+const Input: FC<InputProps> = ({ text }) => {
+  if (text) {
+    console.log(123);
+  }
+
+  return (
+    <section>
+      <button>jj {text}</button>
+      <input type="text" />
+      <img src="" alt="" />
+    </section>
+  );
+};
 
 export default Input;
