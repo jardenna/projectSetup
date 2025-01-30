8341402fa2cf0f506bcf36954e7a3e26b690a053
jardenna <hbjornum@gmail.com>
2025-01-30 10:41:47 +0100

    eslint

--- eslint.config.js
+++ eslint.config.js
@@ -93,8 +93,6 @@ export default tseslint.config(
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-duplicate-enum-values': 'warn',
       '@typescript-eslint/no-empty-interface': 'warn',
-      '@typescript-eslint/no-unnecessary-condition': 'off',
-
       'import/no-extraneous-dependencies': [
         'error',
         {
@@ -123,10 +121,7 @@ export default tseslint.config(
       'react/no-multi-comp': ['error', { ignoreStateless: false }],
       'react/no-unescaped-entities': 0,
       'no-underscore-dangle': ['error', { allow: ['__esModule', '__extends'] }],
-      'import/no-unresolved': 'off',
       'import/extensions': 'off',
-      'react/jsx-uses-react': 'off',
-      'react/react-in-jsx-scope': 'off',
       'react/prop-types': 'off',
       'jsx-a11y/alt-text': 'error',
       'react/jsx-filename-extension': [
@@ -146,19 +141,14 @@ export default tseslint.config(
         { terms: ['todo', 'fixme', 'any other term'], location: 'anywhere' },
       ],
       camelcase: 'off',
-      'react/destructuring-assignment': 'off',
       'react/button-has-type': 'error',
       'react/prefer-stateless-function': 'warn',
       'react/jsx-props-no-spreading': 'off',
       'react/no-did-update-set-state': 'warn',
       'class-methods-use-this': 'off',
       'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
-      'react/no-array-index-key': 'off',
       'react/no-direct-mutation-state': 1,
       'no-nested-ternary': 'warn',
-      'no-prototype-builtins': 'off',
-      'react/sort-comp': 'off',
-      'no-shadow': 'off',
       'react/require-default-props': [
         'error',
         { ignoreFunctionalComponents: true },
